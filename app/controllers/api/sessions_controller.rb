54210ab4d36e4bc7720968a11bd43bf0ff0753e5
Ian <Iokeef@gmail.com>
2017-05-15 17:03:13 -0400

    Add Auth part 1

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,25 @@
+class Api::SessionsController < ApplicationController
+
+  def create
+      @user = User.find_by_credentials(params[:user][:username], params[:user][:password])
+      if @user
+        log_in(@user)
+        render "api/users/show"
+      else
+        render json: ["Invalid Username/Password"], status: 422
+      end
+    end
+
+    def destroy
+      if ( logged_in? )
+        log_out
+        render(json: {})
+      else
+        render(json: ["Not logged in!"], status: 404 )
+      end
+    end
+
+  def user_params
+    params.require(:user).permit(:username, :password, :email_address, :f_name, :l_name)
+  end
+end
