d7fed9c33266beb0894428b006e22aad26f82a57
Ian <Iokeef@gmail.com>
2017-05-17 15:16:55 -0400

    hook up session api

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -15,10 +15,5 @@ class Api::UsersController < ApplicationController
 
   def show
     @user = current_user
-    if @user.update_attributes(user_params)
-      render: show
-    else
-      render json: @user.errors.full_messages, status: 422
-    end
   end
 end
--- frontend/index.jsx
+++ frontend/index.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {logIn, logOut, signUp} from './util/session_api_util';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById("root");
+  window.login = logIn;
+  window.logout = logOut;
+  window.signup = signUp;
   ReactDOM.render(<h1> Welcome to Bench BnB </h1>, root);
 });
