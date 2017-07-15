a387273522ebb3326999900b36ee2251ddc3d5e3
Ian <Iokeef@gmail.com>
2017-07-15 16:33:06 -0400

    Add to index

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -4,6 +4,14 @@ import {logIn, logOut, signUp} from './util/session_api_util';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById("root");
+  let store;
+  if(window.currentUser) {
+    const preloadedState = { session: { currentUser: window.currentUser, errors: [] }};
+    store = configureStore(preloadedState);
+  }
+  else {
+    store = configureStore();
+  }
   window.login = logIn;
   window.logout = logOut;
   window.signup = signUp;
