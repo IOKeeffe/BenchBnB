6a12483755ad7169b28885f343e05cf6a30f5baf
Ian <Iokeef@gmail.com>
2017-07-16 18:50:17 -0400

    Add logger

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,8 +1,9 @@
-import { createStore, applyMiddleware } from 'redux';
-import RootReducer from '../reducers/root_reducer';
+import { createStore, applyMiddlewarem } from 'redux';
+import logger from 'redux-logger';
+import rootReducer from '../reducers/root_reducer';
 import thunk from '../middleware/thunk';
 
 const configureStore = (preloadedState = {}) => {
-  return createStore(RootReducer, preloadedState, applyMiddleware(thunk));
+  return createStore(rootReducer, preloadedState, applyMiddleware(thunk, logger));
 };
 export default configureStore;
