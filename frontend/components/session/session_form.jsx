82a46ee8445a7151ae5f88c2a48f312014b915c4
Ian <Iokeef@gmail.com>
2017-07-24 13:16:56 -0400

    Add state updating for session form

--- frontend/components/session/session_form.jsx
+++ frontend/components/session/session_form.jsx
@@ -15,13 +15,32 @@ class SessionForm extends React.Component {
     this.props.processForm(user);
   }
 
+  update(field) {
+    return e => this.setState({
+      [field]: e.currentTarget.value
+    });
+  }
+
   render() {
     return (
       <div>
         <h1>{this.props.formType}</h1>
         <form className="sign-up-form" onSubmit={this.handleSubmit}>
-
-          <input type="text"></input>
+          <label>User name:
+            <input type="text"
+                   className="username"
+                   value={this.state.username}
+                   onChange={this.update('username')}
+             />
+          </label>
+          <label>Password:
+          <input type="text"
+                 className="password"
+                 value={this.state.password}
+                 onChange={this.update('password')}
+           />
+          </label>
+          <input type="submit" id="submit-  button"></input>
         </form>
       </div>
     )
