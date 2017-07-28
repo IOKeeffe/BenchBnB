56a32ffe08603a7805e2a5cfa4166dcb8eff6079
Ian <Iokeef@gmail.com>
2017-07-28 12:47:31 -0400

    Add bench model

---
+++ app/models/bench.rb
@@ -0,0 +1,14 @@
+# == Schema Information
+#
+# Table name: benches
+#
+#  id          :integer          not null, primary key
+#  description :string           not null
+#  lat         :float            not null
+#  lon         :float            not null
+#
+
+class Bench < ApplicationRecord
+
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170711213052) do
+ActiveRecord::Schema.define(version: 20170728141307) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "benches", force: :cascade do |t|
+    t.string "description", null: false
+    t.float  "lat",         null: false
+    t.float  "lon",         null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username",                        null: false
