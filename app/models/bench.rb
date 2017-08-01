a84120deb89d805b2f59097c56f2e01a218c79d0
Ian <Iokeef@gmail.com>
2017-08-01 18:35:26 -0400

    add owner association to bench model

--- app/models/bench.rb
+++ app/models/bench.rb
@@ -10,6 +10,10 @@
 
 class Bench < ApplicationRecord
   validates :description, :lat, :lon, presence: true
-
+  
+  belongs_to :owner,
+    class_name: "User",
+    foreign_key: owner_id,
+    primary_key: id
 
 end
