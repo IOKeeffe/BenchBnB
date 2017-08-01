6108bc7dd7685555a1a7661df33bc936c153f0cf
Ian <Iokeef@gmail.com>
2017-08-01 17:47:49 -0400

    Add bench seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+benches = {
+  1: {
+    lat: 37.752651,
+    lon: -122.459065
+  },
+  2: {
+    lat: 37.753657,
+    lon:-122.446444
+  },
+  3: {
+      lat: 37.793875,
+      lon:-122.464075
+    }
+}
