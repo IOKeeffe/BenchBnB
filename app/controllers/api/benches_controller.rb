e589814d57411bd1f68ea21aa1fe300bb764766d
Ian <Iokeef@gmail.com>
2017-08-07 10:49:15 -0400

    Add bench api views

--- app/controllers/api/benches_controller.rb
+++ app/controllers/api/benches_controller.rb
@@ -10,6 +10,7 @@ class Api::BenchesController < ApplicationController
 
   def index
     @benches = Bench.all
+    render :index
   end
 
   def new
--- frontend/actions/bench_actions.js
+++ frontend/actions/bench_actions.js
@@ -3,7 +3,7 @@ import * as BenchApiUtil from '../util/bench_api_util';
 export const RECEIVE_BENCHES = "RECEIVE_BENCHES";
 
 export const fetchBenches = () => dispatch => {
-  return BenchApiUtil.fetchBenches.then(benches => {
+  return BenchApiUtil.fetchBenches().then(benches => {
     dispatch(receiveBenches(benches));
   });
 }
