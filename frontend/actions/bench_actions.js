55c6e612bad32e9d58d64c7cea412e070459eb03
Ian <Iokeef@gmail.com>
2017-08-07 10:42:54 -0400

    Add to window

--- frontend/actions/bench_actions.js
+++ frontend/actions/bench_actions.js
@@ -5,7 +5,7 @@ export const RECEIVE_BENCHES = "RECEIVE_BENCHES";
 export const fetchBenches = () => dispatch => {
   return BenchApiUtil.fetchBenches.then(benches => {
     dispatch(receiveBenches(benches));
-  })
+  });
 }
 
 export const receiveBenches = benches => {
--- frontend/index.jsx
+++ frontend/index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/Root'
 import { logIn } from './actions/session_actions';
-import { fetchBenches } from './util/bench_api_util';
+import { fetchBenches } from './actions/bench_actions';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById("root");
