330c2432d88f8f7db192bd5fea35b3a8051b6567
Ian <Iokeef@gmail.com>
2017-08-07 10:28:12 -0400

    finish receive benches

--- frontend/actions/bench_actions.js
+++ frontend/actions/bench_actions.js
@@ -1,5 +1,16 @@
 import * as BenchApiUtil from '../util/bench_api_util';
 
-export const receiveBenches = (benches) => {
+export const RECEIVE_BENCHES = "RECEIVE_BENCHES";
 
+export const fetchBenches = () => dispatch => {
+  return BenchApiUtil.fetchBenches.then(benches => {
+    dispatch(receiveBenches(benches));
+  })
+}
+
+export const receiveBenches = benches => {
+  return {
+    type: RECEIVE_BENCHES,
+    benches
+  }
 }
