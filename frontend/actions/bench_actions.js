9e1c75bfeb4e1f1ccb0557bc2223ffe31b19b5e5
Ian <Iokeef@gmail.com>
2017-08-14 12:01:34 -0400

    Add fetching action

--- frontend/actions/bench_actions.js
+++ frontend/actions/bench_actions.js
@@ -1,16 +1,25 @@
 import * as BenchApiUtil from '../util/bench_api_util';
 
 export const RECEIVE_BENCHES = "RECEIVE_BENCHES";
+export const FETCHING_BENCHES = "FETCHING_BENCHES";
 
 export const fetchBenches = () => dispatch => {
   return BenchApiUtil.fetchBenches().then(benches => {
     dispatch(receiveBenches(benches));
+    dispatch(fetchingBenches(true));
   });
 }
 
 export const receiveBenches = benches => {
   return {
     type: RECEIVE_BENCHES,
-    benches
+    benches,
+  }
+}
+
+export const fetchingBenches = fetching => {
+  return {
+    type: FETCHING_BENCHES,
+    fetching,
   }
 }
