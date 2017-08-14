9116a800b2bd5453b5f19cd82864bf0f2711b81d
Ian <Iokeef@gmail.com>
2017-08-14 12:29:20 -0400

    Add loading to root reducer

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import SessionReducer from './session_reducer';
 import BenchesReducer from './benches_reducer';
+import LoadingReducer from './loading_reducer';
 
 export default combineReducers({
   session: SessionReducer,
   benches: BenchesReducer,
+  loading: LoadingReducer,
 });
