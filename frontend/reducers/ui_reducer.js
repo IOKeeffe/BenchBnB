bba5b946e6bfa20fa4695da64079a1fc722bda34
Ian <Iokeef@gmail.com>
2017-08-14 12:15:22 -0400

    Finish loading reducer

--- frontend/reducers/ui_reducer.js
+++ frontend/reducers/ui_reducer.js
@@ -1,11 +1,19 @@
 import merge from 'lodash/merge';
 
-const UiReducer = (oldState = {loading: false}, action) => {
+import { FETCHING_BENCHES, RECEIVE_BENCHES } from './benches_reducer';
+
+const LoadingReducer = (oldState = {loading: false}, action) => {
   Object.freeze(oldState);
 
   const newState = merge({}, oldState);
 
   switch(action.type) {
-
+    case FETCHING_BENCHES:
+      newState.loading = true;
+    case RECEIVE_BENCHES:
+      newState.loading = true;
   }
+  return newState;
 }
+
+export default LoadingReducer;
